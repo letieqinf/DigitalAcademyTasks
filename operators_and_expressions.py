b6fd4ec6566549a9fa298ad845ea88c82acd4e7c
letieqinf <errlidie@gmail.com>
2022-10-26 16:07:25 +0700

    commit

--- operators_and_expressions.py
+++ operators_and_expressions.py
@@ -39,26 +39,45 @@ def reverse_int32(number: int) -> int:
 
 
 def main():
-    num1, num2, num3 = [randint(0, 100) for i in range(3)]
-    result = average(num1, num2, num3)
-    print(result)
-
-    to_div, div = list(map(int, input('Enter a dividend and a divider: ').split(' ')))
-    result = divide(to_div, div)
-    print(result)
-
-    to_round = float(input('Enter any float number: '))
-    print(round_number(to_round, 1), round_number(to_round, 2), round_number(to_round, 3))
-
-    to_rev = int(input('Enter a number you want to reverse: '))
-    result = reverse(to_rev)
-    print(result)
-
-    to_rev = int(input('Enter a number you want to reverse: '))
-    result = reverse_int32(to_rev)
-    print(result)
+    while True:
+        try:
+            option = int(input('Which task you want to see?\n'
+                               '1. Average\n'
+                               '2. Divide\n'
+                               '3. Round\n'
+                               '4. Reverse\n'
+                               '5. Reverse int32\n'
+                               '0. Quit\n'
+                               'Enter your choice: '))
+            match option:
+                case 1:
+                    num1, num2, num3 = [randint(0, 100) for i in range(3)]
+                    result = average(num1, num2, num3)
+                    print(result)
+                case 2:
+                    to_div, div = list(map(int, input('Enter a dividend and a divider: ').split(' ')))
+                    result = divide(to_div, div)
+                    print(result)
+                case 3:
+                    to_round = float(input('Enter any float number: '))
+                    print(round_number(to_round, 1),
+                          round_number(to_round, 2),
+                          round_number(to_round, 3))
+                case 4:
+                    to_rev = int(input('Enter a number you want to reverse: '))
+                    result = reverse(to_rev)
+                    print(result)
+                case 5:
+                    to_rev = int(input('Enter a number you want to reverse: '))
+                    result = reverse_int32(to_rev)
+                    print(result)
+                case 0:
+                    break
+                case _:
+                    print('\nUndefined option: Try again\n')
+        except ValueError:
+            pass
 
 
 if __name__ == '__main__':
     main()
-
